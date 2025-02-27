712662ba175d3d5c72d4568f4a473ab72876130f
karyagfi <karymeagfi15@gmail.com>
2025-02-26 21:42:22 -0600

    Modifique y agregue cosas a la clase

--- libro.py
+++ libro.py
@@ -1,18 +1,25 @@
 class Libro:
-    def __init__(self, titulo, autor, genero, isbn):
+    def __init__(self, titulo = "Sin titulo", autor = "Sin autor", genero = "Sin genero", isbn = "Sin isbn"):
         self.titulo = titulo
         self.autor = autor
         self.genero = genero
         self.isbn = isbn
+        self.disponible = True
 
     def info(self):
-        return f"El libro '{self.titulo}' fue escrito por {self.autor}, pertenece al género {self.genero} y su ISBN es {self.isbn}."
+        print(f"El libro '{self.titulo}' fue escrito por {self.autor}, pertenece al género {self.genero} y su ISBN es {self.isbn}.")
     
     def prestar(self):
-        return f"El libro '{self.titulo}' ha sido prestado."
+        if self.disponible:
+            self.disponible = False
+            print(f"El libro '{self.titulo}' ha sido prestado.")
+        else:
+            print(f"El libro '{self.titulo}' no está disponible.")
 
     def devolver(self):
-        return f"El libro '{self.titulo}' ha sido devuelto."
+        if not self.disponible:
+            self.disponible = True
+            print(f"El libro '{self.titulo}' ha sido devuelto.")
 
     def disponible(self):
         self.disponible = True
